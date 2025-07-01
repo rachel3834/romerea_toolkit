ed38244efdb3b4242f0505c198d29178a4eaafcb
Alaina Schweitzer <alainamila@icloud.com>
2025-07-01 14:22:42 -0400

    .

--- scatterplots_n_lcs.py
+++ scatterplots_n_lcs.py
@@ -187,10 +187,13 @@ for kind, idx in example_stars.items():
         photometry = photometry[sort]
 
         #saving all photometry columns for chosen stars in txt file
-        full_phot_file = os.path.join(output_dir, f"{kind}_star_{idx}_filter_{flt}.txt")
+        full_phot_file = os.path.join(output_dir, f"{kind}_star_{idx}_filter_{flt}_photometry_cols.txt")
         header = "HJD\tInst_Mag\tInst_Mag_Err\tCalib_Mag\tCalib_Mag_Err\tCorr_Mag\tCorr_Mag_Err\tNorm_Mag\tNorm_Mag_Err\tPhot_Scale\tPhot_Scale_Err\tStamp_Idx\tSky_Bkgd\tSky_Bkgd_Err\tResidual_X\tResidual_Y\tQC_Flag"
-        np.savetxt(full_phot_file, photometry, fmt="%.6f", header=header, delimiter="\t")
-        print(f"Saved star {idx} photometry data to a .txt file in {output_dir}!")
+        try:
+            np.savetxt(full_phot_file, photometry, fmt="%.6f", header=header, delimiter="\t")
+            print(f"Saved star {idx} photometry data to a .txt file in {output_dir}!")
+        except Exception as e:
+            print(f"Failed to save txt: {e}")
 
         print(f"{kind.title()} star {idx}, filter {flt}:")
         print(f"Mag error stats — min: {np.nanmin(errs)}, max: {np.nanmax(errs)}, NaNs: {np.isnan(errs).sum()}")
