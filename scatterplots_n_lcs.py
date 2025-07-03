c9a67d7578ff6a9a56f50b8733c793bdcae73049
Alaina Schweitzer <alainamila@icloud.com>
2025-07-03 17:23:57 -0400

    .

--- scatterplots_n_lcs.py
+++ scatterplots_n_lcs.py
@@ -94,7 +94,7 @@ for flt in filters:
     out = sorted(zip(idxs, means, wmeans, werrors, rms_vals, fit_rms, idxs, fields, counts), key=lambda x:x[0])
     with open(os.path.join(output_dir,f"variability_rms_{flt}.txt"),"w") as f:
         f.write("star_idx mean_mag wmean werror RMS fit_rms field_id n_obs\n")
-        for i,m,wm,we,r,fr,fid,nobs in out:
+        for m,wm,we,r,i,fr,fid,nobs in out:
             f.write(f"{i} {m:.4f} {wm:.4f} {we:.4f} {r:.4f} {fr:.4f} {fid} {nobs}\n")
 
   
