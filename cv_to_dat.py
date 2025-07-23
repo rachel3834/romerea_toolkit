0e47aeae004f3c43c2d4dbdc59a7a95015e9c324
Alaina Schweitzer <alainamila@icloud.com>
2025-07-23 00:06:32 -0400

    <= 0 filter

--- cv_to_dat.py
+++ cv_to_dat.py
@@ -14,8 +14,8 @@ required_columns = {"id", "time", "mag", "mag_err", "filter"}
 if not required_columns.issubset(df.columns):
     raise ValueError(f"Missing columns in input CSV. Required: {required_columns}")
 
-#filter out anything < 0 for mag and _err
-df = df[(df["mag"] < 0) & (df["mag_err"] < 0)]
+#filter out anything <= 0 for mag and _err
+df = df[(df["mag"] <= 0) & (df["mag_err"] <= 0)]
 
 #now to loop thru each filter (i, g, r)
 for filt in ["i", "g", "r"]:
